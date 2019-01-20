28d96c8640b43a6d0e28098650a7dc9bfbc64b3c
lribas <lribas@redhat.com>
2019-01-20 11:17:37 -0200

    teste

--- src/main/java/br/com/banestes/eap/api_sample/SampleResource.java
+++ src/main/java/br/com/banestes/eap/api_sample/SampleResource.java
@@ -6,8 +6,6 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
-import org.jboss.ejb3.annotation.SecurityDomain;
-
 @Path("/")
 public class SampleResource {
     @Inject
